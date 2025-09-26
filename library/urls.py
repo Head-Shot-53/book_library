599b725de65225c424a319162aa12688ef15ea25
Head-Shot-53 <denchikmashtack@gmail.com>
2025-09-26 17:14:22 +0300

    +book_update path, +book_delete path

--- library/urls.py
+++ library/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('book/add/', views.book_create, name='book_create'),
     path('book/<slug:slug>/', views.book_detail, name='book_detail'),
+    path('book/update/<slug:slug>/', views.book_update, name='book_update'),
+    path('book/delete/<slug:slug>/', views.book_delete, name='book_delete')
     
 ]
