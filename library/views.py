4b49dd9259357bf680757cada78d03a2da6fa576
Head-Shot-53 <denchikmashtack@gmail.com>
2025-09-26 16:41:51 +0300

    +book_list, +book_detail, +book_create

--- library/views.py
+++ library/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book, Author
+from .forms import BookForm
+from django.utils.text import slugify
 
-# Create your views here.
+def book_list(request):
+    query = request.GET.get('q')
+    books = Book.objects.all()
+    if query:
+        books = books.filter(title__icontains = query)
+    return render(request, 'library/books_list.html', {'books':books})
+
+
+def book_detail(request, slug):
+    book = get_object_or_404(Book, slug = slug)
+    return render(request, 'library/book_detail.html', {'book':book})
+
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            author_name = form.cleaned_data['author_name']
+            author, created = Author.objects.get_or_create(first_last_name = author_name)
+
+            book = Book(
+                title = form.cleaned_data['title'],
+                year = form.cleaned_data['year'],
+                author = author
+            )
+            book.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'library/book_form.html', {'form':form})
+
