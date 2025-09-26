f0b1833d6af32a529c69a533fa59d5eb1b77d6a3
Head-Shot-53 <denchikmashtack@gmail.com>
2025-09-26 17:13:58 +0300

    +book_update, +book_delete

--- library/views.py
+++ library/views.py
@@ -20,16 +20,36 @@ def book_create(request):
         form = BookForm(request.POST)
         if form.is_valid():
             author_name = form.cleaned_data['author_name']
-            author, created = Author.objects.get_or_create(first_last_name = author_name)
+            author, created = Author.objects.get_or_create(first_last_name=author_name)
 
-            book = Book(
-                title = form.cleaned_data['title'],
-                year = form.cleaned_data['year'],
-                author = author
-            )
+            # отримуємо об’єкт книги з форми, але не зберігаємо
+            book = form.save(commit=False)
+            book.author = author
             book.save()
+
+            # якщо є зв’язки many-to-many, їх треба зберегти після save()
+            form.save_m2m()
+
             return redirect('book_list')
     else:
         form = BookForm()
-    return render(request, 'library/book_form.html', {'form':form})
+    return render(request, 'library/book_form.html', {'form': form})
 
+
+def book_update(request, slug):
+    book = get_object_or_404(Book, slug = slug)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_detail', slug = book.slug)
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'library/book_form.html', {'form':form, 'book':book})
+
+def book_delete(request, slug):
+    book = get_object_or_404(Book, slug=slug)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'library/book_confirm_delete.html', {'book':book})
